1d596451002ac85c71b22d4ac7e0b1eba87cd556
danielmarszal <daniel.marszal99@wp.pl>
2020-11-22 20:15:05 +0100

    game.js add public funtion init and private function start game

--- js/game.js
+++ js/game.js
@@ -1,8 +1,11 @@
 var CandyCrush = window.CandyCrush || {};
 
-CandyCrush.Game = (function ($)){
-   var Game = function(){
-	
+CandyCrush.Game = (function ($){
+	var Game = function(){
+		this.init = function(){
+			$("#game-start").on("click", startGame);
+		}
 	};
+	var startGame = function(){};
 	return Game;
 }(jQuery);
